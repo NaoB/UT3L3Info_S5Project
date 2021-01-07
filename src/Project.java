7ad28b904c9121646b7e6240f52e8b6276f27883
Nao Bellavia <bellavia.nao.nb@gmail.com>
2021-01-07 21:24:39 +0100

    Mise a jour de l'arbre

--- src/Project.java
+++ src/Project.java
@@ -69,19 +69,34 @@ public class Project extends JFrame {
 		SensorType EAU = new SensorType("EAU", "m3", 0, 10);
 		SensorType AIR = new SensorType("AIRCOMPRIME", "m3/h", 0, 5);
 		SensorType ELECTRICITE = new SensorType("ELECTRICITE", "kWh", 10, 500);
+<<<<<<< HEAD
 		SensorType TEMPERATURE = new SensorType("TEMPERATURE", "°C", 17, 22);
+=======
+		SensorType TEMPERATURE = new SensorType("TEMPERATURE", "ï¿½C", 17, 22);
+		JButton buttonOk = new JButton("OK");
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 		
-		// Creation fenêtre principale
+		// Creation fenï¿½tre principale
 		JFrame frame = new JFrame("Capteurs du campus");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1200, 900); 
 				
-		// Création, paramétrage et positionnement des composants
-		// Panel pour les deux blocs à  gauche (Temps Reel et Gestion)
+		// Crï¿½ation, paramï¿½trage et positionnement des composants
+		// Panel pour les deux blocs ï¿½ gauche (Temps Reel et Gestion)
 		JSplitPane splitHorizontal = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JPanel(), new ManagementPanel());
 		
 		// Visualisation a posteriori
+<<<<<<< HEAD
 		List<Sensor> sensors = new ArrayList<>();		
+=======
+		//List<Sensor> sensors = Sensor.fetchAll();
+		List<Sensor> sensors = new ArrayList<>();
+		List<Data> list = new ArrayList<>();
+				
+		JLabel label=new JLabel("Visualisation des donnï¿½es a posteriori",JLabel.CENTER);
+		String[] fluids = {"EAU","ELECTRICITE","TEMPERATURE","AIR COMPRIME"};
+		JComboBox<String> fluidList = new JComboBox<>(fluids);
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 		fluidList.setMaximumSize(new Dimension(50,30));
 		SpinnerDateModel model = new SpinnerDateModel();	 
 		model.setValue(date);
@@ -91,7 +106,7 @@ public class Project extends JFrame {
 		JPanel jpanelText = new JPanel();
 		jpanelText.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
-		// Panel pour le précedent et les graphiques
+		// Panel pour le prï¿½cedent et les graphiques
 		JPanel panel = new JPanel();
 		JSplitPane splitVertical = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, splitHorizontal, panel);
 		
@@ -100,12 +115,12 @@ public class Project extends JFrame {
 		buttonOk.addActionListener(new ActionListener(){
 			@Override public void 
 			actionPerformed(ActionEvent e) {
-				// Récupérer valeurs temps
+				// Rï¿½cupï¿½rer valeurs temps
 				Date start,stop;
 				start = (Date) infBound.getValue();
 				stop = (Date) supBound.getValue();
 				
-				// Récupérer fluide
+				// Rï¿½cupï¿½rer fluide
 				String fluid = fluidList.getSelectedItem().toString();
 				switch(fluid) {
 				case "EAU" : 
@@ -114,7 +129,15 @@ public class Project extends JFrame {
 						cac.addActionListener(new ActionListener(){
 							@Override public void 
 							actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
 								updateGraphic(frame, sensors, panel, start, stop, s, cac);	
+=======
+								sensors.add(s);
+								if(sensors.size()>3) {
+									JOptionPane.showMessageDialog(panel, "Attention vous pouvez sï¿½lectionner 3 capteurs maximum ï¿½ la fois ");
+								}
+								showGraphic(frame, sensors, list, panel, cac,start,stop);
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 							}
 						});
 					}
@@ -125,7 +148,15 @@ public class Project extends JFrame {
 						cac.addActionListener(new ActionListener(){
 							@Override public void 
 							actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
 								updateGraphic(frame, sensors, panel, start, stop, s, cac);
+=======
+								sensors.add(s);
+								if(sensors.size()>3) {
+									JOptionPane.showMessageDialog(panel, "Attention vous pouvez sï¿½lectionner 3 capteurs maximum ï¿½ la fois ");
+								}
+								showGraphic(frame, sensors, list, panel, cac,start,stop);
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 							}
 						});
 					}
@@ -136,19 +167,46 @@ public class Project extends JFrame {
 						cac.addActionListener(new ActionListener(){
 							@Override public void 
 							actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
 								updateGraphic(frame, sensors, panel, start, stop, s, cac);	
+=======
+								sensors.add(s);
+								if(sensors.size()>3) {
+									JOptionPane.showMessageDialog(panel, "Attention vous pouvez sï¿½lectionner 3 capteurs maximum ï¿½ la fois ");
+								}
+								showGraphic(frame, sensors, list, panel, cac,start,stop);
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 							}
 						});		
 					}
 					break;
-				default : // air comprimé
+				default : // air comprimï¿½
 					for (Sensor s : AIR.getSensors()) {
 						JCheckBox cac = showCheckBox(buttonOk, label, fluidList, infBound, supBound, panel, s);	
 						cac.addActionListener(new ActionListener(){
 							@Override public void 
 							actionPerformed(ActionEvent e) {
+<<<<<<< HEAD
 									updateGraphic(frame, sensors, panel, start, stop, s, cac);
 								
+=======
+								sensors.add(s);
+								if(sensors.size()>3) {
+									JOptionPane.showMessageDialog(panel, "Attention vous pouvez sï¿½lectionner 3 capteurs maximum ï¿½ la fois ");
+								}
+								showGraphic(frame, sensors, list, panel, cac,start,stop);
+							}
+						});
+						
+						cac.addChangeListener(new ChangeListener(){
+							@Override public void 
+							stateChanged(ChangeEvent e) {
+								if(cac.isSelected()) {
+										
+								}else {
+									
+								}
+>>>>>>> acc3170 (Mise a jour de l'arbre)
 							}
 						});
 					}
@@ -187,7 +245,7 @@ public class Project extends JFrame {
 		line2.add(new JLabel("Fluide : "));
 		line2.add(fluidList);
 		line2.add(Box.createHorizontalGlue());
-		line2.add(new JLabel("Date début : "));
+		line2.add(new JLabel("Date dï¿½but : "));
 		line2.add(infBound);
 		line2.add(Box.createHorizontalGlue());
 		line2.add(new JLabel("Date fin : "));
